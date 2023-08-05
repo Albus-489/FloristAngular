76b54e20bc12deba7d9722d95a718d8d78bf86dc
Albus-489 <bavrzar@gmail.com>
2023-08-05 09:59:46 +0300

    propcat page

---
+++ src/app/pages/propcataloguepage/propcataloguepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propcataloguepage',
+  templateUrl: './propcataloguepage.component.html',
+  styleUrls: ['./propcataloguepage.component.scss']
+})
+export class PropcataloguepageComponent {
+
+}
