09af5cc784ca1c4dd206c7b7c495417ada3761fd
Albus-489 <bavrzar@gmail.com>
2023-08-05 10:00:06 +0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { PropcataloguepageComponent } from './pages/propcataloguepage/propcataloguepage.component';
+import { BtnOutlineComponent } from './components/btn-outline/btn-outline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    PropcataloguepageComponent,
+    BtnOutlineComponent
   ],
   imports: [
     BrowserModule,
